647925f2a562917d90befedd2e29aa6620b061de
Bradyn <btflahar@asu.edu>
2025-10-25 23:27:52 -0700

    First Commit: Features 1-3 on GraphDot Project Working:

---
+++ src/main/java/btflahar/asu/edu/graphDot/GraphApp.java
@@ -0,0 +1,105 @@
+package btflahar.asu.edu.graphDot;
+
+import org.jgrapht.graph.DefaultDirectedGraph;
+import guru.nidi.graphviz.engine.*;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.engine.GraphvizCmdLineEngine;
+
+public class GraphApp {
+    private Graph<String, DefaultEdge> graph;
+
+    public GraphApp() {
+        graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+        Graphviz.useEngine(new GraphvizCmdLineEngine());
+    }
+
+    //TASK 1 - parseGraph
+    public void parseGraph(String filepath) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(filepath));
+
+        for (String line : lines) {
+            line = line.trim();
+
+            if (line.contains("->")) { //check if line contains arrow for node count/splitting function
+                String[] parts = line.split("->");
+                String src = parts[0].replaceAll("[^a-zA-Z0-9]", "").trim();
+                String dst = parts[1].replaceAll("[^a-zA-Z0-9]", "").trim();
+
+                addNode(src); //add node and add edge for feature 2/3
+                addNode(dst);
+                addEdge(src, dst);
+            }
+
+            else if (line.matches("[a-zA-Z0-9_]+;")) {
+                String node = line.replace(";", "").trim();
+                addNode(node);
+            }
+        }
+    }
+    //TASK 2 - Add node to graph
+    public void addNode(String label) {
+        if (!graph.containsVertex(label)) {
+            graph.addVertex(label);
+        }
+
+        else { //check for duplicate labels:
+            System.out.println("Duplicate: " + label); //duplicate text for test
+        }
+    }
+
+    //TASK 2 -add a list of nodes, just reuse add node
+    public void addNodes(String[] labels) {
+        for (String label : labels) {
+            addNode(label);
+        }
+    }
+    //TASK 3 -  Add edge and check for duplicate edges
+    public void addEdge(String srcLabel, String dstLabel) {
+        if (graph.containsEdge(srcLabel, dstLabel)) {
+            System.out.println("Duplicate: " + srcLabel + " -> " + dstLabel); //duplicate text for test
+        }
+        else {
+            graph.addEdge(srcLabel, dstLabel);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder str1 = new StringBuilder();
+
+        str1.append("Nodes = ").append(graph.vertexSet().size()).append("\n");
+        str1.append("Edges = ").append(graph.edgeSet().size()).append("\n");
+
+        for (DefaultEdge e : graph.edgeSet()) {
+            str1.append(graph.getEdgeSource(e)).append(" -> ").append(graph.getEdgeTarget(e)).append("\n");
+        }
+
+        return str1.toString();
+    }
+
+
+    public void outGraph(String filepath) throws IOException {
+        Files.writeString(Paths.get(filepath), toString());
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        GraphApp app = new GraphApp();
+
+        app.addNodes(new String[]{"A", "B", "C"});
+        app.addEdge("A", "B"); //test graph for test cases
+        app.addEdge("B", "C");
+
+        String outDot = "C:/Users/brady/Desktop/out.dot";
+
+        String outPng = "C:/Users/brady/Desktop/input.png";
+
+
+    }
+}
